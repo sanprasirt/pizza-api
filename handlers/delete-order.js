153dc1f0bdd62ff106a01ba222ecd7c92520ac4b
Sanprasirt boonma <kuforce@gmail.com>
2019-11-27 15:07:53 +0700

    😆 add new func

--- handlers/delete-order.js
+++ handlers/delete-order.js
@@ -1,8 +1,21 @@
-function deleteOrder(id) {
-    if (!id)
-        throw new Error('Order ID is required for deleting the order')
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-    return {}
+function deleteOrder(orderId) {
+    return docClient.delete({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: orderId
+        }
+    }).promise()
+    .then((result) => {
+        console.log('Order is deleted!', result)
+        return result
+    })
+    .catch((deleteError) => {
+        console.log(`Oops, order is ont deleted :(`, deleteError)
+        throw deleteError
+    })
 }
 
 module.exports = deleteOrder
